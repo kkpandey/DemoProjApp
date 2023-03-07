fac3158a66d8b15a781a1c517e7a0bda2f072ad7
Krishna Kant Pandey <krishnakant.pandey@powerservtech.com>
2023-03-07 20:05:00 +0530

    changes

---
+++ DemoProjApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using DemoProjApp.Model;
+using DemoProjApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<PricingRules> pricingRulesList = new List<PricingRules>();
+            PriceingItems priceingItems = new PriceingItems();
+            pricingRulesList = priceingItems.GetPricingRules();
+            ProductCheckOutService storeCheckoutObj = new ProductCheckOutService(pricingRulesList);
+
+            storeCheckoutObj.scan("ipd");
+            storeCheckoutObj.scan("ipd");
+            storeCheckoutObj.scan("ipd");
+            storeCheckoutObj.scan("ipd");
+            storeCheckoutObj.scan("ipd");
+            //Bye 3 Apple Tv
+            storeCheckoutObj.scan("atv");
+            storeCheckoutObj.scan("atv");
+            storeCheckoutObj.scan("atv");
+          
+            storeCheckoutObj.scan("mbp");
+            storeCheckoutObj.scan("mbp");
+            //Amount Payed to users
+            storeCheckoutObj.total();
+        }
+    }
+}
